a64621ff271f0501d3878f80404c88ae6ce1be99
Cristian Buffa <bufface@gmail.com>
2020-05-28 15:16:28 -0300

    6. Implementing a Attack Method

--- 4.The_Monster_Slayer/app.js
+++ 4.The_Monster_Slayer/app.js
@@ -10,6 +10,31 @@ new Vue({
       this.gameIsRunning = true
       this.monsterHealth = 100
       this.playerHealth = 100
-    }
+    },
+    attack: function() {
+      var max = 10
+      var min = 3
+      var damage = Math.max(Math.floor(Math.random() * max), min)
+      this.monsterHealth -= damage
+
+      if (this.monsterHealth <= 0) {
+        alert('You won')
+        this.gameIsRunning = false
+        return
+      }
+
+      var max = 12
+      var min = 5
+      var damage = Math.max(Math.floor(Math.random() * max), min)
+      this.playerHealth -= damage
+
+      if (this.playerHealth <= 0) {
+        alert('You lost')
+        this.gameIsRunning = false
+      }
+    },
+    specialAttack: function() {},
+    heal: function() {},
+    giveUp: function() {}
   }
 })
