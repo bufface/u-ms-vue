4c584544431d5c5cc274b456649741af84c3e838
Cristian Buffa <bufface@gmail.com>
2020-05-30 11:43:47 -0300

    8. Default Slots and Slot Defaults

--- comp-advanced/src/App.vue
+++ comp-advanced/src/App.vue
@@ -4,7 +4,7 @@
       <div class="col-xs-12">
         <app-quote>
           <h2 slot="title">{{ quoteTitle }}</h2>
-          <p slot="content">The ugly quote</p>
+          <p>The ugly quote</p>
         </app-quote>
       </div>
     </div>
