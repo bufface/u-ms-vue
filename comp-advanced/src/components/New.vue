caf713e2fcea6f44fda471bcc199588c37654f1e
Cristian Buffa <bufface@gmail.com>
2020-05-30 11:59:22 -0300

    11. Understanding Dynamic Component Behavior

--- comp-advanced/src/components/New.vue
+++ comp-advanced/src/components/New.vue
@@ -1,5 +1,21 @@
 <template>
   <div>
     <h3>New Quote</h3>
+    <button @click="counter++">Inrease</button>
+    <hr>
+    <p>{{ counter }}</p>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  destroyed() {
+    console.log('🔥')
+  }
+}
+</script>
