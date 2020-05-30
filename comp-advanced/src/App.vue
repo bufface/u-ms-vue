de598657be8ee8192c1e1456c03e661aa6d8947b
Cristian Buffa <bufface@gmail.com>
2020-05-30 11:28:06 -0300

    4. Passing Content with Slots

---
+++ comp-advanced/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <app-quote>
+          <h2>Quote</h2>
+          <p>The ugly quote</p>
+        </app-quote>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Quote from './components/Quote.vue'
+
+export default {
+  components: {
+    appQuote: Quote
+  }
+}
+</script>
+
+<style>
+</style>
