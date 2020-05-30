b432e2a39729a68e4fea70e472c9ef7aae1c4729
Cristian Buffa <bufface@gmail.com>
2020-05-30 11:32:03 -0300

    5. How Slot Content gets Compiled and Styled

--- comp-advanced/src/App.vue
+++ comp-advanced/src/App.vue
@@ -3,7 +3,7 @@
     <div class="row">
       <div class="col-xs-12">
         <app-quote>
-          <h2>Quote</h2>
+          <h2>{{ quoteTitle }}</h2>
           <p>The ugly quote</p>
         </app-quote>
       </div>
@@ -15,6 +15,11 @@
 import Quote from './components/Quote.vue'
 
 export default {
+  data() {
+    return {
+      quoteTitle: 'The Quote'
+    }
+  },
   components: {
     appQuote: Quote
   }
