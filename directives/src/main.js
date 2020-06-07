55844bc6436afda93d0aa3217dbcdadaa38021bc
Cristian Buffa <bufface@gmail.com>
2020-06-07 10:36:32 -0300

    4. Creating a Simple Directive

--- directives/src/main.js
+++ directives/src/main.js
@@ -3,7 +3,12 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
-// Vue.directive('highlight')
+Vue.directive('highlight', {
+  // eslint-disable-next-line
+  bind( el, binding, vnode) {
+    el.style.backgroundColor = 'green'
+  }
+})
 
 new Vue({
   render: h => h(App),
